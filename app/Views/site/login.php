43a8d4bf8cffdef51fbae13612b5beeb2d82ca82
VladislavGorbunov <75179847+VladislavGorbunov@users.noreply.github.com>
2023-05-25 14:01:53 +0300

    Добавлена форма авторизации

--- app/Views/site/login.php
+++ app/Views/site/login.php
@@ -1,30 +1,113 @@
 <!DOCTYPE html>
 <html lang="ru">
-<?php // view('shared/head') ?>
 
-<body class="text-center" style="background: url(https://images.unsplash.com/photo-1444723121867-7a241cacace9?ixlib=rb-1.2.1&auto=format&fit=crop&w=1953&q=80) center/cover #004494; position: relative">
-    <?php // view('home/styling') ?>
-    <div class="justify-content-center container d-flex flex-column" style="min-height: 100vh; max-width: 476px">
-        <p class="mt-5"><a href="/"><img src="/logo_dark.png" alt="Logo" width="150px"></a></p>
-        <form method="POST" name="loginForm" class="container shadow d-flex flex-column justify-content-center pb-1 pt-3 text-white">
-            <h1 class="mb-4">Enter to Portal</h1>
-            <input type="text" name="email" placeholder="Email" class="form-control mb-2">
-            <input type="password" name="password" autocomplete="current-password" placeholder="Password" class="form-control mb-2">
-            <input type="submit" value="Sign In" class="btn-primary btn btn-block mb-3">
-            <div class="separator mb-3">Or</div>
-            <a href="/register" class="btn d-flex align-items-center btn-light border-secondary mb-2">
-                <span class="mx-auto">Register with Email</span>
-            </a>
-            <a href="/" class="btn d-flex align-items-center btn-light border-secondary mb-2">
-                <span class="mx-auto">Back</span>
-            </a>
-        </form>
-        <div class="floating">
-            <small>
-                <a href="https://unsplash.com/photos/ukvgqriuOgo" target="_blank" rel="noopener noreferrer">Background by Henning Witzel</a>
-            </small>
+<head>
+<link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD"
+      crossorigin="anonymous">
+
+      <style>
+        body {
+            margin: 0;
+            padding: 0;
+            background: url(https://vsegda-pomnim.com/uploads/posts/2022-04/1648957313_31-vsegda-pomnim-com-p-fon-lesa-foto-31.jpg) no-repeat;
+            height: 100vh;
+            font-family: sans-serif;
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-position: center;
+            overflow: hidden
+        }
+        
+        @media screen and (max-width: 600px;) {
+            body {
+                background-size: cover;
+                : fixed}
+            }
+            
+            #particles-js {
+                height: 100%
+            }
+            
+            .loginBox {
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%,-50%);
+                width: 350px;
+                min-height: 200px;
+                background: #fff;
+                border-radius: 10px;
+                padding: 40px;
+                box-sizing: border-box
+            } 
+            
+            .user {
+                margin: 0 auto;
+                display: block;
+                margin-bottom: 20px
+            } 
+            
+            h3 {
+                margin: 0;
+                padding: 0 0 20px;
+                color: #59238F;
+                text-align: center
+            }
+            
+            .loginBox input{
+                width: 100%;
+                margin-bottom: 20px
+            }
+            
+            .loginBox input[type="text"], .loginBox input[type="password"]{
+                border: none;
+                border-bottom: 2px solid #262626;
+                outline: none;
+                height: 40px;
+                color: #fff;
+                background: transparent;
+                font-size: 16px;
+                padding-left: 20px;
+                box-sizing: border-box
+            }
+            
+            .loginBox input[type="text"]:hover, .loginBox input[type="password"]:hover{
+                color: #42F3FA;
+                border: 1px solid #42F3FA;
+                box-shadow: 0 0 5px rgba(0,255,0,.3), 0 0 10px rgba(0,255,0,.2), 0 0 15px rgba(0,255,0,.1), 0 2px 0 black
+            }
+            
+            .loginBox input[type="text"]:focus, .loginBox input[type="password"]:focus{
+                border-bottom: 2px solid #42F3FA
+            }
+            
+            .inputBox{position: relative}.inputBox span{position: absolute;top: 10px;color: #262626}.loginBox input[type="submit"]{border: none;outline: none;height: 40px;font-size: 16px;background: #59238F;color: #fff;border-radius: 20px;cursor: pointer}.loginBox a{color: #262626;font-size: 14px;font-weight: bold;text-decoration: none;text-align: center;display: block}a:hover{color: #00ffff}p{color: #0000ff}
+        </style>
+
+
+
+</head>
+
+<body>
+
+<div class="loginBox"> 
+        <h3>Войти</h3>
+        <form action="login.php" method="post">
+            <div class="inputBox"> 
+                <input id="uname" type="text" name="Username" placeholder="Username"> 
+                <input id="pass" type="password" name="Password" placeholder="Password"> </div>
+                <input type="submit" name="" value="Login">
+        </form> 
+        <a href="#">Forget Password<br> </a>
+        <div class="text-center">
+            <p style="color: #59238F;">Sign-Up</p>
         </div>
+        
     </div>
+
 </body>
 
 </html>
