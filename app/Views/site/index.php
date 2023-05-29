6b06c7156bd74818907b88ef88585fe8c397180b
VladislavGorbunov <limitorg2016@ya.ru>
2023-05-29 21:17:51 +0300

    Вёрстка главной

--- app/Views/site/index.php
+++ app/Views/site/index.php
@@ -1,5 +1,32 @@
 <div class="container">
   
+<div class="row mb-4">
+    <div class="col-lg-3">
+        <img src="/public/theme/img/free.svg" width="64px" class="d-block mx-auto">
+        <h6 class="text-center mt-2"><b>Бесплатное бронирование путёвок</b></h6>
+        <p class="text-center">Мы не берём комиссию за бронирование, вы видите и цены напрямую.</p>
+    </div>
+
+    <div class="col-lg-3">
+        <img src="/public/theme/img/handshake.svg" width="64px" class="d-block mx-auto">
+        <h6 class="text-center mt-2"><b>Бесплатное бронирование путёвок</b></h6>
+        <p class="text-center">Мы не берём комиссию за бронирование, вы видите и цены напрямую.</p>
+    </div>
+
+    <div class="col-lg-3">
+        <img src="/public/theme/img/free.svg" width="64px" class="d-block mx-auto">
+        <h6 class="text-center mt-2"><b>Бесплатное бронирование путёвок</b></h6>
+        <p class="text-center">Мы не берём комиссию за бронирование, вы видите и цены напрямую.</p>
+    </div>
+
+    <div class="col-lg-3">
+        <img src="/public/theme/img/free.svg" width="64px" class="d-block mx-auto">
+        <h6 class="text-center mt-2"><b>Бесплатное бронирование путёвок</b></h6>
+        <p class="text-center">Мы не берём комиссию за бронирование, вы видите и цены напрямую.</p>
+    </div>
+</div>
+
+
 <h2 class="text-center">Лучшие детские лагеря России</h2>
 <p class="text-center">На основании голосов и отзывов наших пользователей</p>
 
