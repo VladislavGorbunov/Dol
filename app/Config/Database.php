b4cdbab88c2014fe1e3a119f5f11601634fd77b3
VladislavGorbunov <limitorg2016@ya.ru>
2023-06-13 17:42:59 +0300

    Переименование файла env в .env
    Можно переключаться с development на production

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -28,7 +28,7 @@ class Database extends Config
         'DSN'      => '',
         'hostname' => 'localhost',
         'username' => 'root',
-        'password' => 'root',
+        'password' => 'rootf',
         'database' => 'Dol',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
